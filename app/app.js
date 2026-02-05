c433073125a9d80d5157ba148123ca9f3e9b80a4
samvithvaddiparthi-ship-it <samvith.vaddiparthi@gmail.com>
2026-02-05 20:49:01 +0530

    Implement clean architecture with controllers

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const resumeRoutes = require("../routes/resumeRoutes");
+const authRoutes = require("../routes/authRoutes");
 
 const app = express();
-
 app.use(express.json());
 
 app.use("/api/resume", resumeRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend is running 🚀" });
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const app = require("./app/app");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
